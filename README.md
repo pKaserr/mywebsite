1623b3c5ac946cdf8d868be9c289d381fc78d1bd
Z3r0cks <48802394+Z3r0cks@users.noreply.github.com>
2023-03-24 00:02:32 +0100

    WIP: Slider v.0.8

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mywebsite
+TODO: Slider
+   Point size on mobile
+   Point Active
+   Testing
+
+TODO: Header
+   Menu
+   Size
+   Style
--- public/main.js
+++ public/main.js
@@ -1,14 +1,54 @@
-// starting after html is loaded
-window.addEventListener('load', function () {
+window.addEventListener('load', () => {
+
+   // Slider
    const sliderBtnLeft = document.querySelector('#sliderBtnLeft');
    const sliderBtnRight = document.querySelector('#sliderBtnRight');
-   const sliderImage = document.querySelector('.slider__img');
+   const allSliderImages = document.querySelectorAll('.slider__img');
+   const sliderWrapper = document.querySelector('.slider__wrapper');
+   const sliderPointWrapper = document.querySelector('.slider__pointWrapper');
 
-   sliderBtnLeft.addEventListener('click', function () {
-      console.log('left');
-   });
-   sliderBtnRight.addEventListener('click', function () {
-      console.log('right');
+   // slider points
+   allSliderImages.forEach(el => {
+      const newPoint = document.createElement('div');
+      newPoint.classList.add('slider__point');
+      newPoint.addEventListener('click', () => {
+         showImage(el);
+      });
+      sliderPointWrapper.appendChild(newPoint)
    });
 
+   // slider buttons
+   sliderWrapper.style.height = allSliderImages[currentSliderIndex()].clientHeight + 'px';
+   sliderBtnLeft.addEventListener('click', () => {
+      currentSliderIndex();
+      const currentImgIndex = currentSliderIndex();
+      showAdjacentImage(currentImgIndex, 'left');
+   });
+   sliderBtnRight.addEventListener('click', () => {
+      const currentImgIndex = currentSliderIndex();
+      showAdjacentImage(currentImgIndex, 0);
+   });
+
+   showAdjacentImage = (index, direction) => {
+      allSliderImages[index].classList.add('opacity-0');
+      if (direction === 'left')
+         nextIndex = index === 0 ? allSliderImages.length - 1 : index - 1;
+      else
+         nextIndex = index === allSliderImages.length - 1 ? 0 : index + 1;
+      allSliderImages[nextIndex].classList.remove('opacity-0');
+   };
+
+   showImage = image => {
+      allSliderImages.forEach(el => {
+         el.classList.add('opacity-0');
+      });
+      image.classList.remove('opacity-0');
+   };
+
+   function currentSliderIndex() {
+      for (let i = 0; i < allSliderImages.length; i++) {
+         if (allSliderImages[i].classList.contains('opacity-0')) continue;
+         return i
+      }
+   };
 });
